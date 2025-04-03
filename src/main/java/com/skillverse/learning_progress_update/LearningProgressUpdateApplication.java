8d6803270d61940829db1664518ed4cc3487ae7d
user <it22271082@my.sliit.lk>
2025-04-03 08:09:13 +0530

    initial setup

---
+++ src/main/java/com/skillverse/learning_progress_update/LearningProgressUpdateApplication.java
@@ -0,0 +1,13 @@
+package com.skillverse.learning_progress_update;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningProgressUpdateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningProgressUpdateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skillverse/learning_progress_update/LearningProgressUpdateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skillverse.learning_progress_update;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningProgressUpdateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
